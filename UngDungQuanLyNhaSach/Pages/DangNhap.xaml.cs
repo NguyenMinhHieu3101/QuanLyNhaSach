9e12bcff4be87d941f5f78db326046964fd350e3
MinhHieu <hieu31012002@gmail.com>
2022-11-13 02:12:35 +0700

    UI Đăng nhập, Quên mật khẩu, Thay đổi quy định, Phân quyền

---
+++ UngDungQuanLyNhaSach/Pages/DangNhap.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace UngDungQuanLyNhaSach.Pages
+{
+    /// <summary>
+    /// Interaction logic for DangNhap.xaml
+    /// </summary>
+    public partial class DangNhap : Window
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+        private void Forget_Password_TextBlock(object sender, RoutedEventArgs e)
+        {
+            QuenMatKhau quenMatKhau = new QuenMatKhau();
+            quenMatKhau.Show();    
+            this.Hide();
+        }
+        private void Sign_In_Button(object sender, RoutedEventArgs e)
+        {
+            Home home = new Home();
+            home.Show();
+            this.Hide();
+        }
+        private void Cancel_Button(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Hide();
+        }
+
+
+    }
+}
--- UngDungQuanLyNhaSach/Pages/QuenMatKhau.xaml.cs
+++ UngDungQuanLyNhaSach/Pages/QuenMatKhau.xaml.cs
@@ -10,23 +10,26 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using UngDungQuanLyNhaSach.Pages;
 
-namespace UngDungQuanLyNhaSach
+namespace UngDungQuanLyNhaSach.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for QuenMatKhau.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class QuenMatKhau : Window
     {
-        public MainWindow()
+        public QuenMatKhau()
         {
             InitializeComponent();
         }
-
-        private void Button_SignIn_Click(object sender, RoutedEventArgs e)
+        private void Confirm_Button(object sender, RoutedEventArgs e)
+        {
+            DangNhap dangnhap = new DangNhap();
+            dangnhap.Show();
+            this.Hide();
+        }
+        private void Cancel_Button(object sender, RoutedEventArgs e)
         {
             DangNhap dangnhap = new DangNhap();
             dangnhap.Show();
