3cae5e00c892fa8236343526bc45853d808edcf8
Hoàng Mai <hgmaidg@gmail.com>
2022-11-22 14:23:43 +0700

    Update Tra Cuu Hoa Don

---
+++ UngDungQuanLyNhaSach/Model/HoaDon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UngDungQuanLyNhaSach.Model
+{
+    public class HoaDon
+    {
+        [ColumnName("#")]
+        public int stt { get; set; }
+
+        [ColumnName("Mã Hóa Đơn")]
+        public String maHoaDon { get; set; }
+
+        [ColumnName("Mã Nhân Viên")]
+        public String maNhanVien { get; set; }
+        [ColumnName("Mã Khách Hàng")]
+        public String maKhachHang { get; set; }
+        [ColumnName("Mã Khuyến Mãi")]
+        public String maKhuyenMai { get; set; }
+        [ColumnName("Ngày Lập Hóa Đơn")]
+        public DateTime ngayLapHD { get; set; }
+
+        [ColumnName("Tổng Tiền Hóa Đơn")]
+        public Decimal tongTienHD { get; set; }
+        
+        //[ColumnName("Người lập hóa đơn")]
+        //public String nguoiLapHD { get; set; }
+
+        public HoaDon(string maHoaDon, string maNhanVien,
+            string maKhachHang, string maKhuyenMai, DateTime ngayLapHD, decimal tongTienHD)
+        {
+            this.maHoaDon = maHoaDon;
+            this.maNhanVien = maNhanVien;
+            this.maKhachHang = maKhachHang;
+            this.maKhuyenMai = maKhuyenMai;
+            this.ngayLapHD = ngayLapHD;
+            this.tongTienHD = tongTienHD;
+            //this.nguoiLapHD = nguoiLapHD;
+        }
+        public HoaDon(int stt,string maHoaDon, string maNhanVien,
+            string maKhachHang, string maKhuyenMai, DateTime ngayLapHD, decimal tongTienHD)
+        {
+            this.stt = stt;
+            this.maHoaDon = maHoaDon;
+            this.maNhanVien = maNhanVien;
+            this.maKhachHang = maKhachHang;
+            this.maKhuyenMai = maKhuyenMai;
+            this.ngayLapHD = ngayLapHD;
+            this.tongTienHD = tongTienHD;
+            //this.nguoiLapHD = nguoiLapHD;
+        }
+    }
+    
+}
--- UngDungQuanLyNhaSach/Pages/TraCuuHoaDon.xaml.cs
+++ UngDungQuanLyNhaSach/Pages/TraCuuHoaDon.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +24,48 @@ namespace UngDungQuanLyNhaSach.Pages
     /// </summary>
     public partial class TraCuuHoaDon : Page
     {
+        List<HoaDon> hoaDonList = new List<HoaDon>();
         public TraCuuHoaDon()
         {
             InitializeComponent();
+            loadData();
+        }
+        void loadData()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Server=(local);Database=QUANLYNHASACH;Trusted_Connection=Yes;");
+
+                connection.Open();
+                string readString = "select * from HOADON";
+                SqlCommand command = new SqlCommand(readString, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                int count = 0;
+
+                while (reader.Read())
+                {
+                    count++;
+                    hoaDonList.Add(new HoaDon (stt: count, 
+                        maHoaDon: (String)reader["MaHoaDon"],
+                        maNhanVien: (String)reader["MaNhanVien"],
+                        maKhachHang: (String)reader["MaKhachHang"],
+                        maKhuyenMai: (String)reader["MaKhuyenMai"],
+                        ngayLapHD: (DateTime)reader["NgayLapHoaDon"],
+                        tongTienHD: (decimal)reader["TongTienHoaDon"]
+                        //nguoiLapHD: (String)reader["NguoiLapHoaDon"]
+                        ));
+                    danhSachHDTable.ItemsSource = hoaDonList;
+                }
+                connection.Close();
+            }
+            catch (Exception e1)
+            {
+                //MessageBox.Show("db error");
+                MessageBox.Show(e1.Message);
+                
+            }
         }
         private void danhSachHDTable_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
@@ -39,13 +79,13 @@ namespace UngDungQuanLyNhaSach.Pages
         }
         private void chitietHDTable_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
-            var desc = e.PropertyDescriptor as PropertyDescriptor;
-            var att = desc.Attributes[typeof(ColumnNameAttribute)] as ColumnNameAttribute;
-            if (att != null)
-            {
-                e.Column.Header = att.Name;
-                e.Column.Width = new DataGridLength(1, DataGridLengthUnitType.Star);
-            }
+            //var desc = e.PropertyDescriptor as PropertyDescriptor;
+            //var att = desc.Attributes[typeof(ColumnNameAttribute)] as ColumnNameAttribute;
+            //if (att != null)
+            //{
+            //    e.Column.Header = att.Name;
+            //    e.Column.Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+            //}
         }
     }
 }
