8130944993f844053a5d4e87e81cc18aef3b2759
Hoàng Mai <hgmaidg@gmail.com>
2022-11-22 14:54:24 +0700

    update Tra Cuu Phieu Nhap Sach

---
+++ UngDungQuanLyNhaSach/Model/PhieuNhapSach.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UngDungQuanLyNhaSach.Model
+{
+    public class PhieuNhapSach
+    {
+        [ColumnName("#")]
+        public int stt { get; set; }
+
+        [ColumnName("Mã Phiếu Nhập")]
+        public String maPhieuNhap { get; set; }
+
+        [ColumnName("Mã Nhân Viên")]
+        public String maNhanVien { get; set; }
+        [ColumnName("Mã Kho")]
+        public String maKho { get; set; }
+        [ColumnName("Nhà Cung Cấp")]
+        public String nhaCungCap { get; set; }
+        [ColumnName("Ngày Nhập")]
+        public DateTime ngayNhap { get; set; }
+
+        [ColumnName("Tổng Tiền")]
+        public Decimal tongTien { get; set; }
+
+        //[ColumnName("Người lập hóa đơn")]
+        //public String nguoiLapHD { get; set; }
+
+        public PhieuNhapSach(string maPhieuNhap, string maNhanVien,
+            string maKho, string nhaCungCap, DateTime ngayNhap, decimal tongTien)
+        {
+            this.maPhieuNhap = maPhieuNhap;
+            this.maNhanVien = maNhanVien;
+            this.maKho = maKho;
+            this.nhaCungCap = nhaCungCap;
+            this.ngayNhap = ngayNhap;
+            this.tongTien = tongTien;
+        }
+        public PhieuNhapSach(int stt,string maPhieuNhap, string maNhanVien,
+            string maKho, string nhaCungCap, DateTime ngayNhap, decimal tongTien)
+        {
+            this.stt = stt;
+            this.maPhieuNhap = maPhieuNhap;
+            this.maNhanVien = maNhanVien;
+            this.maKho = maKho;
+            this.nhaCungCap = nhaCungCap;
+            this.ngayNhap = ngayNhap;
+            this.tongTien = tongTien;
+        }
+    }
+
+}
+
--- UngDungQuanLyNhaSach/Pages/TraCuuPhieuNhapSach.xaml.cs
+++ UngDungQuanLyNhaSach/Pages/TraCuuPhieuNhapSach.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +23,48 @@ namespace UngDungQuanLyNhaSach.Pages
     /// </summary>
     public partial class TraCuuPhieuNhapSach : Page
     {
+        List <PhieuNhapSach> phieuNhapSachList = new List <PhieuNhapSach>();
         public TraCuuPhieuNhapSach()
         {
             InitializeComponent();
+            loadData();
+        }
+
+        void loadData()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Server=(local);Database=QUANLYNHASACH;Trusted_Connection=Yes;");
+
+                connection.Open();
+                string readString = "select * from PHIEUNHAP";
+                SqlCommand command = new SqlCommand(readString, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                int count = 0;
+
+                while (reader.Read())
+                {
+                    count++;
+                    phieuNhapSachList.Add(new PhieuNhapSach(stt: count,
+                        maPhieuNhap: (String)reader["MaPhieuNhap"],
+                        maNhanVien: (String)reader["MaNhanVien"],
+                        maKho: (String)reader["MaKho"],
+                        nhaCungCap: (String)reader["NhaCungCap"],
+                        ngayNhap: (DateTime)reader["NgayNhap"],
+                        tongTien: (decimal)reader["TongTien"]
+                        ));
+                    danhSachPNSTable.ItemsSource = phieuNhapSachList;
+                }
+                connection.Close();
+            }
+            catch (Exception e1)
+            {
+                //MessageBox.Show("db error");
+                MessageBox.Show(e1.Message);
+
+            }
         }
 
         private void danhSachPNSTable_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
@@ -40,13 +80,13 @@ namespace UngDungQuanLyNhaSach.Pages
 
         private void chiTietPNSTable_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
-            var desc = e.PropertyDescriptor as PropertyDescriptor;
-            var att = desc.Attributes[typeof(ColumnNameAttribute)] as ColumnNameAttribute;
-            if (att != null)
-            {
-                e.Column.Header = att.Name;
-                e.Column.Width = new DataGridLength(1, DataGridLengthUnitType.Star);
-            }
+            //var desc = e.PropertyDescriptor as PropertyDescriptor;
+            //var att = desc.Attributes[typeof(ColumnNameAttribute)] as ColumnNameAttribute;
+            //if (att != null)
+            //{
+            //    e.Column.Header = att.Name;
+            //    e.Column.Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+            //}
         }
     }
 }
