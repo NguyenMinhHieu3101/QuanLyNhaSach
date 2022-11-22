2d39d89bb03c00f752102e8e8aba919335fa089b
Phuong Quyen Le <20520729@gm.uit.edu.vn>
2022-11-22 18:18:05 +0700

    Test báo cáo

--- UngDungQuanLyNhaSach/Pages/BaoCaoDoanhThu.xaml.cs
+++ UngDungQuanLyNhaSach/Pages/BaoCaoDoanhThu.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -27,18 +28,52 @@ namespace UngDungQuanLyNhaSach.Pages
         public BaoCaoDoanhThu()
         {
             InitializeComponent();
-        }
+        }        
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                decimal tongThu = 0;
+                decimal tongChi = 0;
+                DateTime? datepicker1 = dPickerTuNgay.SelectedDate;
+                DateTime? datepicker2 = dPickerDenNgay.SelectedDate;
                 SqlConnection connection = new SqlConnection(@"Server=(local);Database=QUANLYNHASACH;Trusted_Connection=Yes;");
                 connection.Open();
-            }
-            catch
-            {
 
+                if (datepicker1 == null || datepicker2 == null)
+                {
+                    MessageBox.Show("Null òi");
+                }
+                else
+                {
+                    MessageBox.Show(datepicker1.Value.ToString());
+                    string readString1 = "SELECT SUM(TongTienHoaDon) AS TongThu FROM HOADON  WHERE NgayLapHoaDon BETWEEN '" + datepicker1.Value.ToString() + "' AND '" + datepicker2.Value.ToString() + "'";
+
+                    SqlCommand command = new SqlCommand(readString1, connection);
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    tongThu = (decimal)dt.Rows[0]["TongThu"];
+                    txtTongThu.Text = "TỔNG THU: " + tongThu;
+                }
+
+                //string readString2 = "SELECT SUM(ChiPhi) AS TongChi FROM CHITIETBAOCAODOANHTHU";
+                //command = new SqlCommand(readString2, connection);
+                //da = new SqlDataAdapter(command);
+                //dt = new DataTable();
+                //da.Fill(dt);
+                //tongChi = (decimal)dt.Rows[0]["TongChi"];
+                //txtTongChi.Text = "TỔNG CHI: " + tongChi;
+
+                //decimal loiNhuan = tongThu - tongChi;
+                //txtLoiNhuan.Text = "LỢI NHUẬN: " + loiNhuan;
+
+                connection.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
             }
         }
 
